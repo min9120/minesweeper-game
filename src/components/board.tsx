41b5c3cb19ba51517898e594d5c1a0f741504a80
DESKTOP-KNL7IHR\Gyeongmin Kim <a9120a@gmail.com>
2022-04-25 14:06:31 +0900

    [feat] 주변 지뢰 갯수 체크 로직 구현 및 페이지 wrapper

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -3,30 +3,73 @@ import styled from 'styled-components';
 
 function Board() {
   const WIDTH: number = 8;
-  const TOTAL_CELL: number = WIDTH * WIDTH;
   const TOTAL_MINE: number = 10;
-  const mineMap = new Array(TOTAL_CELL).fill(0);
+  const mineMap = Array.from(Array(WIDTH), () => Array(WIDTH).fill(0));
 
   function getRandomInt(max: number) {
     return Math.floor(Math.random() * max);
   }
+  function initRandomMine() {
+    for (let i = 0; i < TOTAL_MINE; i++) {
+      const randomIntX = getRandomInt(WIDTH);
+      const randomIntY = getRandomInt(WIDTH);
 
-  for (let i = 0; i < TOTAL_MINE; i++) {
-    const randomInt = getRandomInt(TOTAL_CELL);
+      if (mineMap[randomIntX][randomIntY] === -1) {
+        i--;
+        continue;
+      }
 
-    if (mineMap[randomInt] === -1) {
-      i--;
-      continue;
+      mineMap[randomIntX][randomIntY] = -1;
     }
+  }
+  function initMineMap() {
+    for (let x = 0; x < WIDTH; x++) {
+      for (let y = 0; y < WIDTH; y++) {
+        let mineCount: number = 0;
+        if (mineMap[x][y] === -1) continue;
+        // 윗줄
+        if (x > 0) {
+          mineMap[x - 1][y] === -1 ? mineCount++ : null;
+          if (y > 0) {
+            mineMap[x - 1][y - 1] === -1 ? mineCount++ : null;
+          }
+          if (y < WIDTH - 1) {
+            mineMap[x - 1][y + 1] === -1 ? mineCount++ : null;
+          }
+        }
+        // 왼쪽
+        if (y > 0) {
+          mineMap[x][y - 1] === -1 ? mineCount++ : null;
+        }
+        // 오른쪽
+        if (y < WIDTH - 1) {
+          mineMap[x][y + 1] === -1 ? mineCount++ : null;
+        }
+        // 아랫줄
+        if (x < WIDTH - 1) {
+          mineMap[x + 1][y] === -1 ? mineCount++ : null;
+          if (y > 0) {
+            mineMap[x + 1][y - 1] === -1 ? mineCount++ : null;
+          }
+          if (y < WIDTH - 1) {
+            mineMap[x + 1][y + 1] === -1 ? mineCount++ : null;
+          }
+        }
 
-    mineMap[randomInt] = -1;
+        mineMap[x][y] = mineCount;
+      }
+    }
   }
 
+  initRandomMine();
+  initMineMap();
   return (
     <Wrapper>
-      {mineMap.map((el, index) => {
-        return <Cell key={`${index}-${el}`}>{el}</Cell>;
-      })}
+      {mineMap.map((elements) =>
+        elements.map((el, index) => {
+          return <Cell key={`${index}-${el}`}>{el}</Cell>;
+        }),
+      )}
     </Wrapper>
   );
 }
@@ -36,12 +79,14 @@ const Wrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(8, 1fr);
   grid-template-rows: repeat(8, 1fr);
-  width: 160px;
-  height: 160px;
+  width: 400px;
+  height: 400px;
   border: solid 1px green;
 `;
 const Cell = styled.button`
   width: 100%;
   height: 100%;
   background-color: #bdbdbd;
+  font-size: 12px;
+  font-weight: bold;
 `;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,21 @@
 import * as React from 'react';
+import styled from 'styled-components';
 import Board from '../components/board';
 
 function MainPage() {
-  return <Board></Board>;
+  return (
+    <Wrapper>
+      <Board></Board>
+    </Wrapper>
+  );
 }
 
 export default MainPage;
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
